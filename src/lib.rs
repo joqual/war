90f4a880ce5725d9d23236efd69d2e68ae926f6e
Joel Alvarez <jalvarez1492@gmail.com>
2025-05-19 18:27:53 -0400

    Move implementations to lib

---
+++ src/lib.rs
@@ -0,0 +1,101 @@
+use rand::seq::SliceRandom;
+use std::process;
+
+// DO NOT CHANGE THESE VALUES
+static DECK_LENGTH: u8 = 52;
+static NUM_VALUES: u8 = 13;
+
+#[derive(Debug)]
+pub enum Suit {
+    Heart,
+    Spade,
+    Diamond,
+    Club,
+}
+
+#[derive(PartialEq, PartialOrd, Debug)]
+pub enum Value {
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
+
+#[derive(Debug)]
+pub struct Deck {
+    pub cards: Vec<Card>,
+}
+
+impl Deck {
+    pub fn build() -> Result<Deck, &'static str> {
+        println!("Build the deck ...");
+
+        let mut cards = Vec::new();
+
+        for i in 0..DECK_LENGTH {
+            let suit: Suit = match i {
+                0..13 => Suit::Club,
+                13..26 => Suit::Diamond,
+                26..39 => Suit::Heart,
+                39..52 => Suit::Spade,
+                _ => {
+                    eprintln!("Error building deck");
+                    process::exit(-1);
+                }
+            };
+
+            let val: Value = match i % NUM_VALUES {
+                0 => Value::Two,
+                1 => Value::Three,
+                2 => Value::Four,
+                3 => Value::Five,
+                4 => Value::Six,
+                5 => Value::Seven,
+                6 => Value::Eight,
+                7 => Value::Nine,
+                8 => Value::Ten,
+                9 => Value::Jack,
+                10 => Value::Queen,
+                11 => Value::King,
+                12 => Value::Ace,
+                _ => {
+                    eprintln!("Error building deck");
+                    process::exit(-1);
+                }
+            };
+
+            cards.push(Card {
+                suit: suit,
+                value: val,
+            });
+
+            Self::shuffle(&mut cards);
+
+            // for i in &cards {
+            //     println!("{:?}", i);
+            // }
+        }
+
+        Ok(Deck { cards })
+    }
+
+    fn shuffle(cards: &mut Vec<Card>) {
+        let mut rng = rand::rng();
+        cards.shuffle(&mut rng)
+    }
+}
+
+#[derive(Debug)]
+pub struct Card {
+    pub suit: Suit,
+    pub value: Value,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 use std::process;
-
-// DO NOT CHANGE THESE VALUES
-static DECK_LENGTH: u8 = 52;
-static NUM_VALUES: u8 = 13;
+use war::Deck;
 
 fn main() {
     println!("The War has begun.");
 
     println!("Press enter to draw.");
 
-    let my_deck: Deck = match Deck::build() {
+    let my_deck: war::Deck = match Deck::build() {
         Ok(d) => d,
         Err(_) => {
             eprintln!("Error: failed to build deck.");
@@ -18,87 +15,4 @@ fn main() {
     };
 
     println!("{:?}", my_deck.cards[0])
-}
-
-#[derive(Debug)]
-enum Suit {
-    Heart,
-    Spade,
-    Diamond,
-    Club,
-}
-
-#[derive(PartialEq, PartialOrd, Debug)]
-enum Value {
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Ten,
-    Jack,
-    Queen,
-    King,
-    Ace,
-}
-
-#[derive(Debug)]
-struct Deck {
-    cards: Vec<Card>,
-}
-
-impl Deck {
-    pub fn build() -> Result<Deck, &'static str> {
-        println!("Build the deck ...");
-
-        let cards = Vec::new();
-
-        for i in 0..DECK_LENGTH {
-            let suit: Suit = match i {
-                0..13 => Suit::Club,
-                13..26 => Suit::Diamond,
-                26..39 => Suit::Heart,
-                39..52 => Suit::Spade,
-                _ => {
-                    eprintln!("Error building deck");
-                    process::exit(-1);
-                }
-            };
-
-            let val: Value = match i % NUM_VALUES  {
-                0 => Value::Two,
-                1 => Value::Three,
-                2 => Value::Four,
-                3 => Value::Five,
-                4 => Value::Six,
-                5 => Value::Seven,
-                6 => Value::Eight,
-                7 => Value::Nine,
-                8 => Value::Ten,
-                9 => Value::Jack,
-                10 => Value::Queen,
-                11 => Value::King,
-                12 => Value::Ace,
-                _ => {
-                    eprintln!("Error building deck");
-                    process::exit(-1);
-                }
-            };
-
-            
-            println!("{i}, {:?}, {:?}", suit, val);
-
-        }
-
-        Ok(Deck { cards })
-    }
-}
-
-#[derive(Debug)]
-struct Card {
-    suit: Suit,
-    value: Value,
 }
